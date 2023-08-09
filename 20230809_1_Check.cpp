1da21465cf68345d0d4fa050c35ae242020870ff
Nobles <msdntxjrhf@naver.com>
2023-08-09 23:14:20 +0900

    20230809_2

--- 20230809_1_Check.cpp
+++ 20230809_1_Check.cpp
@@ -9,9 +9,9 @@ enum rgb { R, G, B };
 
 int dis(rgb color, int max, int index){
     int sum = 0;
-    if(index>=max) return 0;
-    if(DP[index][color]!=0) return DP[index][color];
-    switch (color)
+    if(index>=max) return 0;//범위 지정(outofbound 방지)
+    if(DP[index][color]!=0) return DP[index][color];//이미 수행한 경우 생략(없을 경우 시간초과)
+    switch (color)//입력받은 역순으로 계산(재귀 사용)
     {
     case R:
         sum += min(dis(G, max, index+1), dis(B, max, index+1));
@@ -24,7 +24,7 @@ int dis(rgb color, int max, int index){
         break;
     }
 
-    return DP[index][color] = sum + list[index][color];
+    return DP[index][color] = sum + list[index][color];//수행 여부와 결과 저장용, 결과 출력
 }
 
 
@@ -37,7 +37,7 @@ int main(){
     list[0][G] = 0;
     list[0][B] = 0;
     for(int i = 1; i<num+1; i++){
-        cin >> list[i][R] >> list[i][G] >> list[i][B];
+        cin >> list[i][R] >> list[i][G] >> list[i][B];//초기화
     }
     int red = dis(R, num+1, 1);
     int green = dis(G, num+1, 1);
